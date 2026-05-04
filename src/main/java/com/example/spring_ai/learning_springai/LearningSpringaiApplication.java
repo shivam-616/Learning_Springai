400910b42b56e4b69e38c21c90e5963ef53ac1bc
learning_git <shivamittal616@gmail.com>
2026-05-04 20:08:48 +0530

    first commit

---
+++ src/main/java/com/example/spring_ai/learning_springai/LearningSpringaiApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_ai.learning_springai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningSpringaiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearningSpringaiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/spring_ai/learning_springai/LearningSpringaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_ai.learning_springai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningSpringaiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
